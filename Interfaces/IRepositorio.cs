ba1119dc83968f32882ba947fc055ec75cbecf36
Izair Junior <izairdelima@outlook.com>
2022-01-24 04:51:13 -0300

    Criando um APP simples de cadastro de series em .NET

---
+++ Interfaces/IRepositorio.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Dio.Series;
+using System;
+
+namespace DIO.Series.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);        
+        void Insere(T entidade);        
+        void Exclui(int id);        
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+}
